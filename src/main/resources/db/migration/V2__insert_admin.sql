6936a605e5a160105c0ab317a5039bc1278d7892
Daniil Zmaev <daniil.zmaev@redcollar.ru>
2024-11-19 12:22:55 +0300

    Docker image for keycloak updated

---
+++ src/main/resources/db/migration/V2__insert_admin.sql
@@ -0,0 +1,3 @@
+INSERT INTO management._user (id, keycloak_id, username, email, created_at)
+VALUES ('f0346d54-e695-453a-a042-3751bc693c48', 'a06c950f-fc33-4271-bd72-6c783a75a411', 'admin', 'admin@admin.com',
+        '2024-11-17 16:10:34.577928');
